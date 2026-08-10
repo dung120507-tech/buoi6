396f9c590e07a4666b4708dfbcb6aca74189547b
dungtm <dung120507@gmail.com>
2026-08-10 12:47:55 +0700

    Khai bao bien va tinh tong

---
+++ buoi5-baitap1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=3, b=4, c=7, tong = a+b+c;
+    float trungbinh = tong/3.0;
+    cout << tong << endl << trungbinh;
+    return 0;
+
+}
