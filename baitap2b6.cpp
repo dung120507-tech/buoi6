25f3df33835b7298b786bd7f70c07a621975aca5
dungtm <dung120507@gmail.com>
2026-08-12 00:09:00 +0700

    Kiem tra dieu kien xet tuyen

--- baitap2b6.cpp
+++ baitap2b6.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 using namespace std;
-int main ()
-{
+
+int main() {
     float diem;
     string khuVuc;
-    cout << "Nhap diem thi: ";
-    cin >> diem ;
+    cout << "Nhap diem: ";
+    cin >> diem;
     cout << "Nhap khu vuc: ";
     cin >> khuVuc;
-    if (diem >= 20)
-    {
-        if ( khuVuc == "KV1"){
+
+    if (diem >= 20) {
+        if (khuVuc == "KV1") {
             diem = diem + 0.75;
-        } if (khuVuc == "KV2") {
-            diem = diem + 0.5;
         } else {
-            diem = diem + 0;
+            if (khuVuc == "KV2") {
+                diem = diem + 0.5;
+            } else {
+                diem = diem + 0;
+            }
         }
-        cout << " Diem sau khi cong uu tien la: " << diem;
-    } 
-    else {
-        cout << " Khong du dieu kien xet tuyen";
+
+        cout << "Diem sau khi cong uu tien la: " << diem ;
+    } else {
+        cout << "Khong du dieu kien xet tuyen";
     }
+
     return 0;
 }
--- buoi5-baitap1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int a=3, b=4, c=7, tong = a+b+c;
-    float trungbinh = tong/3.0;
-    cout << tong << endl << trungbinh;
-    return 0;
-
-}
